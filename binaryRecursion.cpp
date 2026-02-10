7eadfb9f0d679cf2c051600c213fd9a43c54b5a6
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2026-02-10 16:35:26 +0530

    Update binaryRecursion.cpp

--- binaryRecursion.cpp
+++ binaryRecursion.cpp
@@ -34,3 +34,38 @@ int main(){
 // ======================================================================
 //                             1
 // ======================================================================
+
+
+
+// Another changes..........................
+
+// Binary Search with Recursion
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binaryRecursion(vector<int> arr,int tar, int st, int end){
+
+    if(st <= end){
+
+        int mid = st +(end - st) / 2;
+        if(tar > arr[mid]){
+            return binaryRecursion(arr,tar,mid+1,end);
+        }
+        else if(tar < arr[mid]){
+            return binaryRecursion(arr,tar,st, mid-1);
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    vector<int> arr={-1,0,3,4,5,9,12};
+    int tar1 = 0;
+    cout<<binaryRecursion(arr,tar1,0, arr.size() - 1)<<endl;
+    return 0;
+}     
